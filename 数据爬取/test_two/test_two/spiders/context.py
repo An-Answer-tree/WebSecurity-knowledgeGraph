32637b84a169e10f312bfbe20956e4642768498f
An-Answer-tree <szliutong@qq.com>
2023-05-06 12:24:41 +0800

    提交

--- 数据爬取/test_two/test_two/spiders/context.py
+++ 数据爬取/test_two/test_two/spiders/context.py
@@ -7,11 +7,11 @@ class ContextSpider(scrapy.Spider):
     name = "context"
     allowed_domains = ["www.secrss.com"]
     # 从CSV文件中读取数据
-    data = np.genfromtxt('/Users/read_me/vscode/vscode Python/面向网络安全事件态势感知的多源异构知识网络/数据爬取/test_one/result.csv', delimiter=',', dtype=str)
+    data = np.genfromtxt('../../test_one/result.csv', delimiter=',', dtype=str)
 
     # 获取包含所有字符串的列表
     string_list = data.tolist()
-    start_urls = string_list[0]
+    start_urls = string_list
 
     def parse(self, response):
         item = TestTwoItem()
