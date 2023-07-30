2f085f803205375a8159db403b425cfde4ed025e
An-Answer-tree <szliutong@qq.com>
2023-07-31 01:00:34 +0800

    初步完成LDA主题划分

--- theme.py
+++ theme.py
@@ -5,23 +5,29 @@ from gensim.corpora import Dictionary
 fp = open('data_without_POS.txt','r',encoding='utf8')
 data_2D_list = eval(fp.read())
 
-train = []
-train.append(data_2D_list[0])
-train.append(data_2D_list[1])
-
+# -------------------------将待训练数据表示为词袋向量----------------------------
+train = data_2D_list
 dictionary = corpora.Dictionary(train)
-
 corpus = [dictionary.doc2bow(text) for text in train]
 
-lda = LdaModel(corpus=corpus, id2word=dictionary, num_topics=20, passes=60)
-# num_topics：主题数目
-# passes：训练伦次
-# num_words：每个主题下输出的term的数目
+# -------------------------训练LDA模型，并将训练出的n个类别存储在文件theme_class.txt中----------------------------
+lda = LdaModel(corpus=corpus, id2word=dictionary, num_topics=10, passes=60)
 
-for topic in lda.print_topics(num_words = 20):
-    termNumber = topic[0]
-    print(topic[0], ':', sep='')
-    listOfTerms = topic[1].split('+')
-    for term in listOfTerms:
-        listItems = term.split('*')
-        print('  ', listItems[1], '(', listItems[0], ')', sep='')
+with open('theme_class.txt', 'wt') as f:
+    for topic in lda.print_topics(num_words = 20):
+        termNumber = topic[0]
+        print(topic[0], ':', file=f)
+        listOfTerms = topic[1].split('+')
+        for term in listOfTerms:
+            listItems = term.split('*')
+            print('  ', listItems[1], '(', listItems[0], ')', file=f)
+
+# -------------------------用训练好的LDA模型对文本进行分类----------------------------
+f2 = open('classify_result.txt', 'wt')
+n = 0
+for i in train:
+    doc_bow = dictionary.doc2bow(i)      #文档转换成bow
+    doc_lda = lda[doc_bow]
+    print(n, ': ', doc_lda, file=f2)
+    n += 1
+f2.close()
